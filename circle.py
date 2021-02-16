71565db7ffeda0992e6c20dbf0f90b101c85ad89
Jabrail Chumakov <67557966+jabrail-chumakov@users.noreply.github.com>
2021-02-16 21:34:21 +0600

    Update circle.py

--- circle.py
+++ circle.py
@@ -25,16 +25,13 @@ def get_settings(path, size):
         avg_colors.append(np.sum(np.sum(image, axis=0), axis=0) / (size ** 2))
     return images, np.array(avg_colors)
 
+def ciclelify(src):
+    cv2.circle(src, round(opt.stride/2),round(opt.stride/2),round(opt.stride/2), (0,0,0), -1)
 
 def main(opt):
     input_image = cv2.imread(opt.input, cv2.IMREAD_COLOR)
-    R = 1
-    n = 64
-    t = np.linspace(0, 2*np.pi, n+1)
-    height = R*np.cos(t)
-    width = R*np.sin(t)
     height, width, num_channels = input_image.shape
-    blank_image = np.zeros((height, width, 3), np.float64)
+    blank_image = np.zeros((height, width, 3), np.uint8)
     images, avg_colors = get_settings(opt.pool, opt.stride)
     for i, j in product(range(int(width / opt.stride)), range(int(height / opt.stride))):
         partial_input_image = input_image[j * opt.stride: (j + 1) * opt.stride,
@@ -42,9 +39,14 @@ def main(opt):
         partial_avg_color = np.sum(np.sum(partial_input_image, axis=0), axis=0) / (opt.stride ** 2)
         distance_matrix = np.linalg.norm(partial_avg_color - avg_colors, axis=1)
         idx = np.argmin(distance_matrix)
-        blank_image[j * opt.stride: (j + 1) * opt.stride, i * opt.stride: (i + 1) * opt.stride, :] = images[idx]
-    cv2.imwrite(opt.output, blank_image)
 
+        radius = 100.0/(distance_matrix[idx]/2+100)
+        circle = np.zeros((round(opt.stride), round(opt.stride)), dtype=images[idx].dtype)
+        cv2.circle(circle, (round(opt.stride/2),round(opt.stride/2)) ,round(radius*opt.stride/2), 1, -1)
+        masked_data = images[idx] * circle[..., np.newaxis]
+
+        blank_image[j * opt.stride: (j + 1) * opt.stride, i * opt.stride: (i + 1) * opt.stride, :] = masked_data
+    cv2.imwrite(opt.output, blank_image)
 
 if __name__ == '__main__':
     opt = settings()
